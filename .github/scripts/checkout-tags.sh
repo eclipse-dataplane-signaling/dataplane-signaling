78f97fc4bee633a3e71da83d1aff30eded3bf156
ndr_brt <andrea.bertagnolli@gmail.com>
2026-05-22 13:47:51 +0200

    docs: fix schemas 404

--- .github/scripts/checkout-tags.sh
+++ .github/scripts/checkout-tags.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 mkdir -p HEAD
+mv ./specifications/schemas .
+mv * HEAD
+
 git fetch --all --tags
 tags_string=$(git tag)
 echo got tag string
 echo $tags_string
 tags_array=($tags_string)
-mv * HEAD
+
 for tag in "${tags_array[@]}"
 do
   echo starting with tag $tag
@@ -15,6 +18,9 @@ do
   git clone $GITHUB_SERVER_URL/$GITHUB_REPOSITORY.git --depth 1 --branch ${tag} --quiet
   pwd
   mv ./dataplane-signaling/* .
+  mv ./specifications/schemas .
+  rm -rf dataplane-signaling
   cd ..
 done
+
 pwd
